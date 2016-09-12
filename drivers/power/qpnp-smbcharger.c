ae0bc18589e8c59f757bea5f0b7ae12c4901cd22
flar2 <asegaert@gmail.com>
2016-09-12 14:32:02 -0400

    USB fastcharge

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -41,6 +41,10 @@
 #include <linux/ktime.h>
 #include "pmic-voter.h"
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastcharge.h>
+#endif
+
 #ifdef CONFIG_MACH_XIAOMI_KENZO
 int FG_charger_status = 0;
 #endif
@@ -3515,6 +3519,11 @@ static void smbchg_external_power_changed(struct power_supply *psy)
 	if (usb_supply_type != POWER_SUPPLY_TYPE_USB)
 		goto  skip_current_for_non_sdp;
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge)
+		current_limit = 900;
+#endif
+
 	pr_smb(PR_MISC, "usb type = %s current_limit = %d\n",
 			usb_type_name, current_limit);
 
@@ -4376,7 +4385,6 @@ static void smbchg_reg_work(struct work_struct *work)
 		schedule_delayed_work(&chip->reg_work,
 			NOT_CHARGING_PERIOD_MS * HZ);
 }
-
 
 static void smbchg_hvdcp_det_work(struct work_struct *work)
 {
