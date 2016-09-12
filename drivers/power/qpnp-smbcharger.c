83bdc939099ea8b6338729cef824b0f8a1f15550
flar2 <asegaert@gmail.com>
2016-09-12 14:32:02 -0400

    USB fastcharge

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -43,6 +43,9 @@
 #include <linux/thermal.h>
 #include <linux/device.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastcharge.h>
+#endif
 
 int FG_charger_status = 0;
 
@@ -3248,6 +3251,10 @@ static void smbchg_external_power_changed(struct power_supply *psy)
 	if (usb_supply_type != POWER_SUPPLY_TYPE_USB)
 		goto  skip_current_for_non_sdp;
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge)
+		current_limit = 900;
+#endif
 	pr_smb(PR_MISC, "usb type = %s current_limit = %d\n",
 			usb_type_name, current_limit);
 	mutex_lock(&chip->current_change_lock);
